d3410b6de2f9b19573ada381632774ef6ca36393
Jaskamal <codeperitus@gmail.com>
2014-11-20 17:53:55 +0530

    now preview export is working

--- src/Shopline/Bundle/PDFDesignerBundle/Controller/DefaultController.php
+++ src/Shopline/Bundle/PDFDesignerBundle/Controller/DefaultController.php
@@ -5,7 +5,8 @@ namespace Shopline\Bundle\PDFDesignerBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\HttpFoundation\Response;
+use Oro\Bundle\EmailBundle\Provider\SystemVariablesProvider;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Shopline\Bundle\PDFDesignerBundle\Entity\DesignerTemplate as DesignerTemplate;
@@ -126,9 +127,8 @@ class DefaultController extends Controller
     public function createAction(){
         return $this->update( new DesignerTemplate() );
     }
-
     /**
-     * @Route("/preview/{id}", name="shopline_designertemplate_preview", requirements={"id"="\d+"}, defaults={"id"=0}))
+     * @Route("/preview/{id}/{pdf}", name="shopline_designertemplate_preview", requirements={"id"="\d+"}, defaults={"pdf" = null }))
      * @Acl(
      *      id="shopline_designertemplate_preview",
      *      type="entity",
@@ -139,7 +139,7 @@ class DefaultController extends Controller
      * @param bool|int $id
      * @return array
      */
-    public function previewAction($id = false)
+    public function previewAction($id, $pdf='')
     {
         if (!$id) {
             $designerTemplate = new DesignerTemplate();
@@ -148,22 +148,49 @@ class DefaultController extends Controller
             $em = $this->get('doctrine.orm.entity_manager');
             $designerTemplate = $em->getRepository('Shopline\Bundle\PDFDesignerBundle\Entity\DesignerTemplate')->find($id);
         }
-
-        /** @var FormInterface $form */
-        $form = $this->get('shopline.form.designertemplate');
-        $form->setData($designerTemplate);
-        $request = $this->get('request');
-
-        if (in_array($request->getMethod(), array('POST', 'PUT'))) {
-            $form->submit($request);
-        }   
-   
-        $templateRendered = $this->get('shopline.template_renderer')
-            ->compilePreview($designerTemplate);
-
-        return array(
-            'content'     => $templateRendered
+        
+        $loadedEntity = $em->getRepository($designerTemplate->getEntityName())->findAll();
+        foreach( $loadedEntity as $entity ){
+            $neededEntity = $entity;
+            break;
+        }
+//        $loadedEntity = new $className();
+        
+        $twig = new \Twig_Environment(new \Twig_Loader_String());
+        
+        $content = $designerTemplate->getContent();
+        
+        
+        $str = $designerTemplate->getContent().'';
+        $system = $this->get('oro_email.emailtemplate.variable_provider.system')->getVariableValues();
+        $content = $twig->render($str, array('entity'=> $neededEntity, 'system' => $system));
+        
+        $data = array(
+            'header' => $designerTemplate->getHeader(),
+            'footer' => $designerTemplate->getFooter(),
+            'styleContent'=>$designerTemplate->getStyleContent(),
+            'content'=> $content,
+            'id' => $id,
+            'pdf'=> $pdf,
+            'system' => $system,
+            'entity' => $loadedEntity
         );
+        
+        if( $pdf == 'pdf' ){
+            $html = $this->renderView('ShoplinePDFDesignerBundle:Default:preview.html.twig', $data);
+
+            return new Response(
+                $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+                200,
+                array(
+                    'Content-Type'          => 'application/pdf',
+                    'Content-Disposition'   => 'attachment; filename="'.$designerTemplate->getName().'.pdf"'
+                )
+            );
+        }
+        
+         //$content
+        return $data;
     }
     /**
      * @param DesignerTemplate $entity
@@ -175,7 +202,7 @@ class DefaultController extends Controller
         if ($this->get('shopline.form.handler.designertemplate')->process($entity)) {
             $this->get('session')->getFlashBag()->add(
                 'success',
-                $this->get('translator')->trans('oro.designer.controller.designertemplate.saved.message')
+                $this->get('translator')->trans('oro.email.controller.emailtemplate.saved.message')
             );
 
             return $this->get('oro_ui.router')->redirectAfterSave(
