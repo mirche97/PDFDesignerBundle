3e4918fa89ed714bc1c0445aeec408d370a3970f
Jaskamal <codeperitus@gmail.com>
2015-01-29 15:55:21 +0530

    Update ShoplineBundleInstaller.php

--- src/Shopline/Bundle/PDFDesignerBundle/Migrations/Schema/ShoplineBundleInstaller.php
+++ src/Shopline/Bundle/PDFDesignerBundle/Migrations/Schema/ShoplineBundleInstaller.php
@@ -31,6 +31,35 @@ class ShoplineBundleInstaller implements Installation
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('field', 'string', ['length' => 500]);
         $table->setPrimaryKey(['id']);
+        $table = $schema->createTable('shopline_designer_template');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('user_owner_id', 'integer', []);
+        $table->addColumn('organization_id', 'integer', []);
+        $table->addColumn('definition', 'text', []);
+        $table->addColumn('isSystem', 'smallint', ['length' => 1]);
+        $table->addColumn('isEditable', 'smallint', ['length' => 1]);
+        $table->addColumn('name', 'string', ['length' => 255]);
+        $table->addColumn('parent', 'integer', []);
+        $table->addColumn('header', 'text', ['notnull' => false]);
+        $table->addColumn('content', 'text', ['notnull' => false]);
+        $table->addColumn('style_content', 'text', ['notnull' => false]);
+        $table->addColumn('footer', 'text', ['notnull' => false]);
+        $table->addColumn('entityName', 'string', ['length' => 255]);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', []);
+        $table->addColumn('type', 'string', ['length' => 20]);
+        $table->setPrimaryKey(['id']);
+        //$table->addUniqueIndex(['user_owner_id', 'organization_id'], 'unique_idx');
+    //    $table->addIndex(['user_owner_id'], 'IDX_AB2BC195A76ED395', []);
+        /** End of generate table oro_sidebar_state **/
 
+        /** Generate table oro_sidebar_widget **/
+        $table = $schema->createTable('shopline_designer_template_translation');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('object_id', 'integer', []);
+        $table->addColumn('locale', 'string', ['length' => 8]);
+        $table->addColumn('field', 'string', ['length' => 50]);
+        $table->addColumn('content', 'text', ['notnull' => false]);
+        $table->setPrimaryKey(['id']);
     }
 }
