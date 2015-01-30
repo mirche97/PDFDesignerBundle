68037e87485d1c0c2556d62269cb067bbaed5d29
Jaskamal <codeperitus@gmail.com>
2015-01-30 13:43:51 +0530

    Update DesignerTemplateTranslation.php

--- src/Shopline/Bundle/PDFDesignerBundle/Entity/DesignerTemplateTranslation.php
+++ src/Shopline/Bundle/PDFDesignerBundle/Entity/DesignerTemplateTranslation.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
 
 /**
- * @ORM\Table(name="shopline_designer_template_translation", indexes={
+ * @ORM\Table(name="designer_template_translate", indexes={
  *      @ORM\Index(name="lookup_unique_idx", columns={"locale", "object_id", "field"})
  * })
  * @ORM\Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
