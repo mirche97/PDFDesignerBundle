deda3cd7e23792731e7ae38c60526c8ed60de392
Ashwani Sharma <ashwani@agilecontinental.com>
2015-02-04 17:04:11 +0530

    update

---
+++ src/Shopline/Bundle/PDFDesignerBundle/Migrations/Data/ORM/LoadDefaultPDFDesigner.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shopline\Bundle\PDFDesignerBundle\Migrations\Schema;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+
+class LoadDefaultPDFDesigner extends AbstractFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+    }
+}
